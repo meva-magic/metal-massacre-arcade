2015925395e9da24cbee7ba7f10d57c56c28bb82
peachomon <saharov.dev@gmail.com>
2023-05-13 10:05:51 +0300

    should fix the tail wag

--- strawberry dragon/Assets/Scripts/BodyRotation.cs
+++ strawberry dragon/Assets/Scripts/BodyRotation.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BodyRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+
+    private Vector2 direction;
+    public Transform target;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+
+        direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
         
     }
 }
